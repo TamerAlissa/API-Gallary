21c2a7fb85fd67ae71b6fd2ccd800d1f6c7a23b3
tamer <tameralissa25@gmail.com>
2019-11-04 09:37:10 +0200

    edit

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ let http = require('http');
 let fun = require('./server');
 let server = http.createServer(fun);
 server.listen(3000 , '127.0.0.4' , ()=>{
-    console.log('Tamer')
+    // console.log('Tamer')
 })

9477e01e55aa3c229d260b12d55ed342b899d103
tamer <tameralissa25@gmail.com>
2019-11-04 15:14:20 +0200

    edit5

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-let http = require('http');
-let fun = require('./server');
-let server = http.createServer(fun);
-server.listen(3000 , '127.0.0.4' , ()=>{
-    // console.log('Tamer')
-})
+var photoArray = [];
+var page = 1;
+data();
+function data() {
+    const myheaders = new Headers();
+    fetch('http://localhost:3000/photos/' + page++, {
+            method: 'GET',
+        })
+        .then(Response => Response.json())
+        .then(result => {
+            photoArray = photoArray.concat(result);
+            addData();
+        });
+}
+
+function addData() {
+    console.log(photoArray);
+    var doc = photoArray.map((photo, index) => {
+        // console.log(photo);
+        return `<img class="cl" src="${photo.url}" data-id="${index}" onClick="togglePhoto(event)">`;
+    });
+    document.getElementById('test').innerHTML = doc.join('');
+}

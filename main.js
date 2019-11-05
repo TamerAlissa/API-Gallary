5da1fc47d31f48b97d2999fda9c7d9f1d1a77b64
tamer <tameralissa25@gmail.com>
2019-11-05 12:47:27 +0200

    txtFile

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-var photoArray = [];
+// var photoArray = [];
+let div = document.getElementById("test");
+let tr;
+let fd;
 var page = 1;
 data();
 function data() {
-    const myheaders = new Headers();
-    fetch('http://localhost:3000/photos/' + page++, {
+    // const myheaders = new Headers();
+    fetch('http://localhost:4000/photos/' + page, {
             method: 'GET',
         })
         .then(Response => Response.json())
         .then(result => {
-            photoArray = photoArray.concat(result);
-            addData();
+          tr = result.photos
+          fd = tr.length
+          page++
+          for(let i=0 ; i<fd ; i++) {
+    div.innerHTML += `<img src = "${tr[i]}" class = "img"/>`
+          }
         });
 }
 
-function addData() {
-    console.log(photoArray);
-    var doc = photoArray.map((photo, index) => {
-        // console.log(photo);
-        return `<img class="cl" src="${photo.url}" data-id="${index}" onClick="togglePhoto(event)">`;
-    });
-    document.getElementById('test').innerHTML = doc.join('');
-}
+// function addData() {
+//     console.log(photoArray);
+//     var doc = photoArray.map((photo, index) => {
+//         // console.log(photo);
+//         return `<img class="cl" src="${photo.url}" data-id="${index}" onClick="togglePhoto(event)">`;
+//     });
+//     document.getElementById('test').innerHTML = doc.join('');
+// }
+// photoArray = photoArray.concat(result);
